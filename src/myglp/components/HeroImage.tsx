7ad04a470e19b302c0777e3cedc1a52454ce28b5
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2025-07-02 10:08:13 -0700

    Feat: Set up ESLint and fix linting issues

--- src/myglp/components/HeroImage.tsx
+++ src/myglp/components/HeroImage.tsx
@@ -1,9 +1,10 @@
 
+import Image from 'next/image';
 import styles from './HeroImage.module.css';
 
 const HeroImage = () => {
   return (
-    <img src="/myglp-hero-image.png" alt="MyGLP App Screenshot" className={styles.heroImage} />
+    <Image src="/myglp-hero-image.png" alt="MyGLP App Screenshot" className={styles.heroImage} width={500} height={300} />
   );
 };
 
