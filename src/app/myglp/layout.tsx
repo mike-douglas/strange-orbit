20a1b38308082748541ac9651045b0def693681a
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2025-07-02 10:56:42 -0700

    Center aligned layout

--- src/app/myglp/layout.tsx
+++ src/app/myglp/layout.tsx
@@ -1,6 +1,8 @@
+import styles from './MyGLPLayout.module.css';
+
 export default function MyGLPLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
+    <main className={styles.container}>
       {children}
     </main>
   );
