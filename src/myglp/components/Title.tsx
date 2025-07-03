c28757f56cfb830547f35cee60e9e14c5037736e
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2025-07-03 01:24:21 -0700

    Update Title: fixing typo

--- src/myglp/components/Title.tsx
+++ src/myglp/components/Title.tsx
@@ -2,6 +2,6 @@ import styles from './Title.module.css';
 
 export default function Title() {
   return (
-    <h1 className={styles.title}>GLP-1 tracking app that you&apos;ll love.</h1>
+    <h1 className={styles.title}>A GLP-1 tracking app that you&apos;ll love.</h1>
   );
 }
