712fd539f66996a0344488c5296806606171a9d4
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2025-07-02 17:34:11 -0700

    Adding new images

---
+++ src/strange-orbit/AboutMe.tsx
@@ -0,0 +1,12 @@
+export default function AboutMe() {
+  return (
+    <section>
+      <article>
+        <h2>Hi, my name is Mike. 👋</h2>
+        <p>I&apos;m an iOS developer that started my journey with GLP-1 in early 2025. In that time, I tried a lot of different apps to track my weight, symptoms, and general mood. None of them did what I wanted so in a weekend I built the first version of MyGLP.</p>
+        <p>At the same time, I left my day job in tech to persue my dream of being an independent software developer doing what I love to do most: build apps that make lives better.</p>
+        <p>I launched MyGLP mid-2025 and I&apos;m excited to continue improving it for me and everyone who finds it useful. Hopefully you do too.</p>
+      </article>
+    </section>
+  )
+}
